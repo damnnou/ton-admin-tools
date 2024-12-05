9c1f93da119b7374903d53e36d589291f4ea9899
damnnou <skdamn@outlook.com>
2024-12-05 11:05:44 +0300

    fix: build to public folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const config = {
         transaction: './src/tools/transaction/transaction.ts',        
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
     },
     devServer: {
         open: true,
